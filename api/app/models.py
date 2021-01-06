52558d3139b68aa341fb2f1de1cfe1963a42727a
Esli Heyvaert <esli.heyvaert@gmail.com>
2021-01-06 09:10:50 +0100

    Docker secrets

--- api/app/models.py
+++ api/app/models.py
@@ -4,12 +4,20 @@ from sqlalchemy import (Column, Integer, String,
                         create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from glob import glob
+
+# Overwrite env vars with a secret if available
+for var in glob('/run/secrets/*'):
+    k = var.split('/')[-1].upper()
+    v = open(var).read().rstrip('\n')
+    os.environ[k] = v
+    # print(f'export {k}={v}')
 
 pg_user = os.getenv('PG_USER', 'postgres')
 pg_host = os.getenv('PG_HOST', 'localhost')
 pg_port = os.getenv('PG_PORT', '5432')
 pg_database = os.getenv('PG_DATABASE', 'postgres')
-pg_password = os.getenv('PG_PASSWORD', 'postgres_password')
+pg_password = os.getenv('PG_PASSWORD', 'password')
 
 engine = create_engine(
     'postgresql://' + pg_user + ':' + pg_password + '@' + pg_host + ':' + pg_port + '/' + pg_database
--- web/webapp/config.py
+++ web/webapp/config.py
@@ -1,8 +1,16 @@
 import os
+from glob import glob
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    # Overwrite env vars with a secret if available
+    for var in glob('/run/secrets/*'):
+        k = var.split('/')[-1].upper()
+        v = open(var).read().rstrip('\n')
+        os.environ[k] = v
+        # print(f'export {k}={v}')
+
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
     STATIC_FOLDER = os.getenv('STATIC_FOLDER', '/static')
     DEBUG = os.getenv('DEBUG', False)
     ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
