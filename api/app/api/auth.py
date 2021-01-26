07f3dda5754658c315f9f8190bbc449ea58e3e72
Esli Heyvaert <esli.heyvaert@gmail.com>
2021-01-26 18:24:52 +0100

    Forgot dot!

--- api/app/api/auth.py
+++ api/app/api/auth.py
@@ -20,7 +20,7 @@ def login_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordR
         db, email=form_data.username, password=form_data.password
     )
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=400, detail="Incorrect email or password.")
     access_token_expires = timedelta(minutes=config.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": create_access_token(
